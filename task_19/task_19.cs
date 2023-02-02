f08f6b29a181544a08d31da6d456c282db694eb8
Alena Savostyanova <el.savostyanova90@yandex.ru>
2023-02-02 23:06:16 +0300

    Homework.

---
+++ task_19/task_19.cs
@@ -0,0 +1,26 @@
+﻿
+// Задача 19: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+string palindrom()
+{
+    System.Console.WriteLine(" Введите 5-и значное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    char[] arr = number.ToString().ToArray();
+    if (arr.Length != 5)
+    {
+        return ("Введите 5-и значное число.");
+    }
+    if (arr[0] == arr[4] && arr[1] == arr[3])
+    {
+        return ("Число является палиндромом.");
+    }
+
+    return ("Число не является палиндромом.");
+}
+System.Console.WriteLine(palindrom());
---
+++ task_23/task_23.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9. 
+// 5 -> 1, 8, 27, 64, 125
+
+string NumberCub()
+{
+    System.Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string result = "";
+    for (int i = 1; i <= number; i++)
+    {
+        result += Math.Pow(i, 3) + " ";
+
+    }
+    return result;
+}
+System.Console.WriteLine(NumberCub());
